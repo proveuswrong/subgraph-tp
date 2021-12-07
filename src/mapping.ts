34f89639e194dccc640e5a3888d9042419386400
Ferit Tunçer <ferit@cryptolab.net>
2021-12-08 01:18:20 +0300

    feat(mapping): balance update

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
   ProveMeWrong,
   BalanceUpdate,
@@ -14,32 +14,30 @@ import {
   TimelockStarted,
   Withdrawal,
   Withdrew
-} from "../generated/ProveMeWrong/ProveMeWrong"
-import { ExampleEntity } from "../generated/schema"
+} from "../generated/ProveMeWrong/ProveMeWrong";
+import { ExampleEntity } from "../generated/schema";
 
 export function handleBalanceUpdate(event: BalanceUpdate): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  let entity = new ExampleEntity(event.transaction.from.toHex());
+  entity.save();
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+  // Entity fields can be set using simple assignments
+  entity.count = BigInt.fromI32(0);
 
   // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  entity.count = entity.count + BigInt.fromI32(1);
 
   // Entity fields can be set based on event parameters
-  entity.claimAddress = event.params.claimAddress
-  entity.newTotal = event.params.newTotal
+  entity.claimAddress = event.params.claimAddress;
+  entity.newTotal = event.params.newTotal;
 
   // Entities can be written to the store with `.save()`
-  entity.save()
+  entity.save();
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
